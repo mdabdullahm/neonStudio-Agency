c9703af4ff213351497f3282bad5fdc87811255c
mdabdullahm <mdabdullahmorol38@gmail.com>
2025-12-27 23:17:48 +0600

    update code

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "i.ibb.co" },
+      { protocol: "https", hostname: "flagcdn.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+    ],
+  },
+};
+
+module.exports = nextConfig;
